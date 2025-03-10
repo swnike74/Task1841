a3b5fe8bf7e5ee9ce9a9fb0809b7baa1b1c772b9
Ведерников Сергей <swnike@mail.ru>
2025-03-10 22:13:41 +0500

    update 2

--- Task1841/Program.cs
+++ Task1841/Program.cs
@@ -12,7 +12,7 @@ namespace Task1841
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
             string videoURL = "https://www.youtube.com/watch?v=-CwJJkAouBw";
@@ -29,24 +29,38 @@ namespace Task1841
             var Duration = video.Duration;
             var Engagement = video.Engagement;
 
-            //var outputFilePath = "C:\\Users\\swnike\\Downloads";
-            var outputFilePath = "C:\\Users\\swnike\\source\\repos\\Task1841\\Task1841\\bin\\Debug\\net8.0";
+            //await client.Videos.DownloadAsync(videoURL, "video.mp4");
 
-            ValueTask vtask = client.Videos.DownloadAsync(Id, outputFilePath, builder => builder.SetPreset(ConversionPreset.UltraFast));
+            //await client.Videos.DownloadAsync(videoURL, "video.mp4", o => o
+            //    .SetContainer("webm") // override format
+            //    .SetPreset(ConversionPreset.UltraFast) // change preset
+            //    //.SetFFmpegPath("path/to/ffmpeg") // custom FFmpeg location
+            //    );//customizing
 
+            //===========Manually selecting streams========================
             var streamManifest = client.Videos.Streams.GetManifestAsync(videoURL);
-            var streamInfo = streamManifest.Result.GetMuxedStreams().GetWithHighestVideoQuality();
-            client.Videos.Streams.DownloadAsync(streamInfo, outputFilePath);
+            //var streamInfo = streamManifest.Result.GetMuxedStreams().GetWithHighestVideoQuality();
+            //var streamInfo = streamManifest.Result.GetVideoStreams().GetWithHighestVideoQuality();
+            var streamInfo = streamManifest.Result.GetAudioStreams().GetWithHighestBitrate();//GetWithHighestVideoQuality();
 
+            //var streamInfo = streamManifest.Result.GetMuxedStreams().GetWithHighestBitrate();//.GetWithHighestBitrate();
 
+            // ...or the highest quality MP4 video-only stream
+            //var streamInfo = streamManifest
+            //    .GetVideoOnlyStreams()
+            //    .Where(s => s.Container == Container.Mp4)
+            //    .GetWithHighestVideoQuality()
 
-            Task task = UserCanDownloadVideo(client, outputFilePath, Id, videoURL);
+            await client.Videos.Streams.DownloadAsync(streamInfo, $"video.{streamInfo.Container}");
+
         }
         public static async Task UserCanDownloadVideo(YoutubeClient youtube, string outputFilePath, VideoId id, string url)
         {
-        //https://ru.stackoverflow.com/questions/1175824/c-%D0%BF%D1%80%D0%BE%D0%B8%D0%B3%D1%80%D1%8B%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5-%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE-%D1%81-youtube
-        //https://learn.microsoft.com/en-us/answers/questions/201990/q-why-isnt-my-app-working
+            //https://ru.stackoverflow.com/questions/1175824/c-%D0%BF%D1%80%D0%BE%D0%B8%D0%B3%D1%80%D1%8B%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5-%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE-%D1%81-youtube
+            //https://learn.microsoft.com/en-us/answers/questions/201990/q-why-isnt-my-app-working
 
+            //https://github.com/Tyrrrz/YoutubeExplode
+            //https://github.com/Tyrrrz/YoutubeExplode/tree/master/YoutubeExplode.Converter
 
             var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
             var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
