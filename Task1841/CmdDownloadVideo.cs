ebdc8c89a75c5f9d4c30c9dbfe83d70d3b759233
Ведерников Сергей <swnike@mail.ru>
2025-03-11 21:50:07 +0500

    update 20250311

--- Task1841/CmdDownloadVideo.cs
+++ Task1841/CmdDownloadVideo.cs
@@ -9,17 +9,14 @@ namespace Task1841
     class CmdDownloadVideo : Command
     {
         Receiver receiver;
-
         public CmdDownloadVideo(Receiver receiver)
         {
             this.receiver = receiver;
         }
-
         public override void Cancel()
         {
             throw new NotImplementedException();
         }
-
         public override void Run()
         {
             Console.WriteLine("Команда загрузки видео отправлена");
--- Task1841/Program.cs
+++ Task1841/Program.cs
@@ -20,11 +20,8 @@ namespace Task1841
             //string videoURL = "https://www.youtube.com/watch?v=-CwJJkAouBw";
             //string videoURL = "https://www.youtube.com/watch?v=i9CPGOTd0G4";
 
-
-
             // создадим отправителя
             var sender = new Sender();
-
             // создадим получателя
             var receiver = new Receiver(videoURL);
 
@@ -44,7 +41,7 @@ namespace Task1841
                 switch (sdecision)
                 {
                     case "N": return;
-                       
+
                     case "Y":
                         var commandTwo = new CmdDownloadVideo(receiver);
                         sender.SetCommand(commandTwo);
@@ -55,68 +52,6 @@ namespace Task1841
                         break;
                 }
             }
-
-
-
-
-
-
-
-            /* var client = new YoutubeClient();
-             var video = client.Videos.GetAsync(videoURL).Result;
-
-             var Id = video.Id;
-             var Title = video.Title;
-             var Description = video.Description;
-             var Author = video.Author.ChannelTitle;
-             var Duration = video.Duration;
-             var Engagement = video.Engagement;
-
-             await client.Videos.DownloadAsync(videoURL, "video.mp4");   */
-
-            /*await client.Videos.DownloadAsync(videoURL, "video.mp4", o => o
-                .SetContainer("webm") // override format
-                .SetPreset(ConversionPreset.UltraFast) // change preset
-                //.SetFFmpegPath("path/to/ffmpeg") // custom FFmpeg location
-                );//customizing */
-
-            //===========Manually selecting streams========================
-            //var streamManifest = client.Videos.Streams.GetManifestAsync(videoURL);
-            //var streamInfo = streamManifest.Result.GetMuxedStreams().GetWithHighestVideoQuality();
-            //var streamInfo = streamManifest.Result.GetVideoStreams().GetWithHighestVideoQuality();
-            //var streamInfo = streamManifest.Result.GetAudioStreams().GetWithHighestBitrate();//GetWithHighestVideoQuality();
-
-            //var streamInfo = streamManifest.Result.GetMuxedStreams().GetWithHighestBitrate();//.GetWithHighestBitrate();
-
-            // ...or the highest quality MP4 video-only stream
-            //var streamInfo = streamManifest
-            //    .GetVideoOnlyStreams()
-            //    .Where(s => s.Container == Container.Mp4)
-            //    .GetWithHighestVideoQuality()
-
-            //await client.Videos.Streams.DownloadAsync(streamInfo, $"video.{streamInfo.Container}");
-
-        }
-
-
-
-        public static async Task UserCanDownloadVideo(YoutubeClient youtube, string outputFilePath, VideoId id, string url)
-        {
-            //https://ru.stackoverflow.com/questions/1175824/c-%D0%BF%D1%80%D0%BE%D0%B8%D0%B3%D1%80%D1%8B%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5-%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE-%D1%81-youtube
-            //https://learn.microsoft.com/en-us/answers/questions/201990/q-why-isnt-my-app-working
-
-            //https://github.com/Tyrrrz/YoutubeExplode
-            //https://github.com/Tyrrrz/YoutubeExplode/tree/master/YoutubeExplode.Converter
-
-            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
-            var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
-
-            await youtube.Videos.Streams.DownloadAsync(streamInfo, outputFilePath);
-
-
-            //await youtube.Videos.DownloadAsync(id, outputFilePath, static builder => builder.SetPreset(ConversionPreset.UltraFast));
-
-            var fileInfo = new FileInfo(outputFilePath);
         }
     }
 }
